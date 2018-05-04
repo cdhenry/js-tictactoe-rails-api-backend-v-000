0eb6d42ebc760af0a6da3dbf501d4c24df37b451
Chris Henry <cdhenry@gmail.com>
2018-05-04 16:52:09 +0000

    automatically backed up by learn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,8 @@
 class GamesController < ApplicationController
-  # Add your GamesController code here
+  before_action :set_game, only: [:show, :update]
+  
   def create
-    @game = Game.create(state: params[:state])
+    @game = Game.create(game_params)
     render json: @game, status: 201
   end
 
@@ -12,7 +13,7 @@ class GamesController < ApplicationController
 
   def update
     @game = Game.find(state: params[:id])
-    @game.update(params[:state])
+    @game.update(game_params)
     render json: @game, status: 202
   end
 
