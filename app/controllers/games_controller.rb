aa16133e88b50fc263a27f0c93330d0825e320f3
Chris Henry <cdhenry@gmail.com>
2018-05-04 16:38:20 +0000

    automatically backed up by learn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,11 +1,13 @@
 class GamesController < ApplicationController
   # Add your GamesController code here
   def create
-    render "home/index"
+    @author = Author.find(params[:id])
+    render json: @author, status: 200
   end
 
   def show
-    render "home/index"
+    @game = Game.find(params[:id])
+    render json: @game, status: 200
   end
 
   def update
@@ -13,6 +15,7 @@ class GamesController < ApplicationController
   end
 
   def index
+    @game = Game.all
     render "home/index"
   end
 end
