6feda73ebfe524ec8b63718f80471ceceed2c26e
Chris Henry <cdhenry@gmail.com>
2018-05-04 16:51:34 +0000

    automatically backed up by learn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -27,7 +27,6 @@ class GamesController < ApplicationController
       @game = Game.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def game_params
       params.require(:game).permit(:state)
     end
