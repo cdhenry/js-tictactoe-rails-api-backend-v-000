acb8ad63f6c649e72da19749509f96b55ad1eaf5
Chris Henry <cdhenry@gmail.com>
2018-05-04 16:08:51 +0000

    automatically backed up by learn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,18 +1,18 @@
 class GamesController < ApplicationController
   # Add your GamesController code here
   def create
-    render :home
+    render "home/index"
   end
 
   def show
-    render :home
+    render "home/index"
   end
 
   def update
-    render :home
+    render "home/index"
   end
 
   def index
-    render :home
+    render "home/index"
   end
 end
